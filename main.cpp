174dc7ee2088c642956586a9fe1ea22a140eef9f
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-04 01:01:54 +0530

    update all

--- main.cpp
+++ main.cpp
@@ -6,9 +6,3 @@ int main(){
     cout << "Hello, World!" << endl;
     return 0;
 }
-=======
-int main() {
-    cout << "Hello, World!";
-    return 0;
-}
->>>>>>> 606fa45a351263442d81ebb21351b84666a8d355

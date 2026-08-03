0eeb514461e3cc217f9a74c2d96acda8dc4d63a3
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-04 01:42:53 +0530

    update all

---
+++ var&dataty.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age = 22; // declare an integer variable 'age' and initialize it with the value 22
+    cout << "my age is: " << age <<endl;
+    return 0;
+}

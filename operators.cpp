8e02d044f90e102395c2f290aebfa315c2e422ca
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-08 23:59:14 +0530

    update all

--- operators.cpp
+++ operators.cpp
@@ -1,1 +1,6 @@
 #include <iostream> // include the iostream library for input/output operations
+using namespace std;  // use the standard namespace to avoid prefixing standard library names with "std::"
+int main(){ // main function where the program execution begins
+    cout << "Hello, World!" << endl; // print "Hello, World!" to the console followed by a newline
+    return 0;  // return 0 to indicate that the program executed successfully
+}  // end of the main function

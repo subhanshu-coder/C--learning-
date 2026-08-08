130c45c6ede8e768c95d66c3774b57d8bb8981c0
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-08 23:59:48 +0530

    update all

--- operators.cpp
+++ operators.cpp
@@ -1,6 +1,10 @@
 #include <iostream> // include the iostream library for input/output operations
 using namespace std;  // use the standard namespace to avoid prefixing standard library names with "std::"
 int main(){ // main function where the program execution begins
-    cout << "Hello, World!" << endl; // print "Hello, World!" to the console followed by a newline
+    int a = 10, b = 3;
+
+cout << a + b << endl;
+cout << a % b << endl;
+cout << (a > b) << endl; << endl; // print "Hello, World!" to the console followed by a newline
     return 0;  // return 0 to indicate that the program executed successfully
 }  // end of the main function

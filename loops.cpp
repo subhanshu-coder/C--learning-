c63b48231134628eebbd670eda5f97b4052c8418
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-09 23:33:10 +0530

    update all

--- loops.cpp
+++ loops.cpp
@@ -5,3 +5,9 @@ for(int i = 1; i <= 10; i++) {
     cout << i << " ";
     return 0;  // return 0 to indicate that the program executed successfully
 }
+
+
+// solving while loop
+#include <iostream> // include the iostream library for input/output operations
+using namespace std;  // use the standard namespace to avoid prefixing standard library names with "
+

c2191f62ae8276b0d36ebeb0e781e02377e2bc5c
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-09 23:36:01 +0530

    update all

--- loops.cpp
+++ loops.cpp
@@ -26,3 +26,14 @@ int main() {
 // do- while loop solving 
 #include <iostream>
 using namespace std;  // use the standard namespace to avoid prefixing standard library names with "
+
+int main() {
+    int i = 1; // initialize the loop counter variable 'i' to 1
+
+    do { // do-while loop that executes the block of code at least once
+        cout << i << " "; // print the current value of 'i' followed by a space
+        i++; // increment the value of 'i' by 1
+    } while(i <= 10); // continue the loop as long as 'i' is less than or equal to 10
+
+    return 0; // return 0 to indicate that the program executed successfully
+}

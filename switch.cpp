8f31efb2ff479889f02ffad3cad9b73bc8679f03
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-09 23:26:53 +0530

    update all

--- switch.cpp
+++ switch.cpp
@@ -1,2 +1,12 @@
 #include <iostream> // include the iostream library for input/output operations
 using namespace std;  // use the standard namespace to avoid prefixing standard library names with "
+
+int day;
+cin >> day;
+
+switch(day) {
+    case 1: cout << "Monday"; break;
+    case 2: cout << "Tuesday"; break;
+    case 3: cout << "Wednesday"; break;
+    default: cout << "Invalid day";
+}

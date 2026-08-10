403d535b7060aa269efbdef3209e45659839113f
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-11 00:01:07 +0530

    update all

--- constructor.cpp
+++ constructor.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+class Student {
+public:
+    string name;
+
+    Student(string n) {
+        name = n;
+    }
+};
+
+int main() {
+    Student s("Rahul");
+
+    cout << s.name;
+}

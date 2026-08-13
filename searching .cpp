0cd00c6354afe095cecd32d72ba85ba3ec8105a8
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-13 23:55:47 +0530

    update all

--- searching .cpp
+++ searching .cpp
@@ -12,3 +12,10 @@
 
 // binary search implementation
 #include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+   

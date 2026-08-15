b7c30e675e5e27c35ce2e9a913429a6a91eec9e4
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-15 23:38:11 +0530

    update all

--- sorting.cpp
+++ sorting.cpp
@@ -1,7 +1,7 @@
 #include <algorithm> // include the algorithm library for sorting algorithms
 #include <vector>
 
-vector<int> arr = {5, 2, 8, 1, 3};
+vector<int> arr = {5, 2, 8, 1, 3}; // Create a vector of integers
 
 sort(arr.begin(), arr.end());
 

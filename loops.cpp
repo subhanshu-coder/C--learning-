cabc502a98ad56795be317c14eaf91f65aa36d59
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-17 23:51:08 +0530

    update all

--- loops.cpp
+++ loops.cpp
@@ -13,7 +13,7 @@ for(int i = 1; i <= 10; i++) {
 using namespace std;  // use the standard namespace to avoid prefixing standard library names with "
 
 int main() {
-    int i = 1; // initialize the loop counter variable 'i' to 1
+    int i = 1; 
 
     while(i <= 10) { // while loop that continues as long as 'i' is less than or equal to 10
         cout << i << " "; // print the current value of 'i' followed by a space

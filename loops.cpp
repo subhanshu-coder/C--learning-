05ca1bc0ba25a9e1f2ae5ea1dae695cc78312ebb
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-17 23:47:00 +0530

    update all

--- loops.cpp
+++ loops.cpp
@@ -30,7 +30,7 @@ using namespace std;  // use the standard namespace to avoid prefixing standard 
 int main() {
     int i = 1; // initialize the loop counter variable 'i' to 1
 
-    do { // do-while loop that executes the block of code at least once
+    do { 
         cout << i << " "; // print the current value of 'i' followed by a space
         i++; // increment the value of 'i' by 1
     } while(i <= 10); // continue the loop as long as 'i' is less than or equal to 10

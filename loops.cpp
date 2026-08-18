6e0c825001262ef62e2395bb37c5f48688b4ad9f
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-18 23:18:10 +0530

    update all

--- loops.cpp
+++ loops.cpp
@@ -10,7 +10,7 @@ for(int i = 1; i <= 10; i++) {
 
 // solving while loop
 #include <iostream> // include the iostream library for input/output operations
-using namespace std;  // use the standard namespace to avoid prefixing standard library names with "
+using namespace std; 
 
 int main() {
     int i = 1; 

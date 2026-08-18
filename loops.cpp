0f27a1dca6dd589a793d7501fbd3a4444106a7a8
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-18 23:21:33 +0530

    update all

--- loops.cpp
+++ loops.cpp
@@ -1,5 +1,5 @@
 #include <iostream> // include the iostream library for input/output operations
-using namespace std;  // use the standard namespace to avoid prefixing standard library names with "
+using namespace std;  
 
 for(int i = 1; i <= 10; i++) {
     cout << i << " ";

76a535c47356f6507f618eda3e0920fc1479c096
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-18 23:18:42 +0530

    update all

--- loops.cpp
+++ loops.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main() {
     int i = 1; 
 
-    while(i <= 10) { // while loop that continues as long as 'i' is less than or equal to 10
+    while(i <= 10) { 
         cout << i << " "; // print the current value of 'i' followed by a space
         i++; // increment the value of 'i' by 1
     }

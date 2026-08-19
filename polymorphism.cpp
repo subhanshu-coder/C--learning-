e9be3090a40b13672aa19b122a6517e0d7d361fd
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-19 23:51:46 +0530

    update all

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Student {  // Class definition for Student
 public: // Public access specifier to allow access to the member variable and constructor from outside the class
-    string name; // Member variable to store the name of the student
+    string name; 
 
     Student(string n) { 
         name = n;  

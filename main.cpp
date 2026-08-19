22e2fad0d0ae9c7437b86fb994ea46642fa30038
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-19 23:44:07 +0530

    update all

--- main.cpp
+++ main.cpp
@@ -4,5 +4,4 @@ using namespace std;  // use the standard namespace to avoid prefixing standard 
 int main(){ // main function where the program execution begins
     cout << "Hello, World!" << endl; // print "Hello, World!" to the console followed by a newline
     return 0;  // return 0 to indicate that the program executed successfully
-}  // end of the main function
-
+}  

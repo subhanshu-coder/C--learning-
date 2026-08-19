9f02b513ea569406dbe36a179104e270a7c3a08a
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-19 23:45:22 +0530

    update all

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream> // include the iostream library for input/output operations
 using namespace std;  // use the standard namespace to avoid prefixing standard library names with "std::"
 
-int main(){ // main function where the program execution begins
+int main(){ 
     cout << "Hello, World!" << endl; 
     return 0;  
 }  

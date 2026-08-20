0b143ada96151a7f6c2ed62809114dcc827d4a7b
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-20 23:46:17 +0530

    update all

--- searching .cpp
+++ searching .cpp
@@ -25,7 +25,7 @@ int binarySearch(int arr[], int size, int target) {  // binary search function i
         if (arr[mid] == target)
             return mid; // Element found
         else if (arr[mid] < target) // If the middle element is less than the target, search in the right half
-            left = mid + 1; // Search in the right half
+            left = mid + 1; 
         else
             right = mid - 1; 
     }

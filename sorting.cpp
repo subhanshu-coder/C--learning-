a4f2c5a8b88895fab1213b37d5ae4446e519841c
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-21 23:18:02 +0530

    update all

--- sorting.cpp
+++ sorting.cpp
@@ -3,7 +3,6 @@
 
 vector<int> arr = {5, 2, 8, 1, 3}; // Create a vector of integers
 
-sort(arr.begin(), arr.end());  // Sort the vector in ascending order using the sort function from the algorithm library
-
+sort(arr.begin(), arr.end()); 
 for(int x : arr) 
     cout << x << " "; 

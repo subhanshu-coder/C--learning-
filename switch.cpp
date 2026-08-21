8b94fd038d75deac7a3b2cba9a224b62d96d5000
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-21 23:21:17 +0530

    update all

--- switch.cpp
+++ switch.cpp
@@ -1,5 +1,5 @@
 #include <iostream> // include the iostream library for input/output operations
-using namespace std;  // use the standard namespace to avoid prefixing standard library names with "
+using namespace std; 
 
 int day;
 cin >> day;

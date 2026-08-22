235df897905a87c5f218ec1d938c3dce81a8e2e2
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-22 23:50:21 +0530

    update all

--- var-datatype.cpp
+++ var-datatype.cpp
@@ -12,7 +12,7 @@ int main() {
     cout << "my age is: " << age <<endl;
     cout << "size of int: " << sizeof(age) << " bytes" << endl; 
     cout << "size of bool: " << sizeof(isStudent) << " bytes" << endl; 
-    cout << "size of float: " << sizeof(height) << " bytes" << endl; // print the size of the float variable 'height'
+    cout << "size of float: " << sizeof(height) << " bytes" << endl;
     cout << "size of char: " << sizeof(grade) << " bytes" << endl; // print the size of the char variable 'grade'
     cout << "size of double: " << sizeof(weight) << " bytes" << endl; // print the size of the double variable 'weight'
     cout << "size of string: " << sizeof(name) << " bytes" << endl; // print the size of the string variable 'name'

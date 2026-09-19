679d689a206967e962f85a12eca8883d3d7e518f
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-19 23:48:26 +0530

    update all

--- sorting.cpp
+++ sorting.cpp
@@ -1,8 +1,8 @@
-#include <algorithm> 
-#include <vector>
+// #include <algorithm> 
+// #include <vector>
 
-vector<int> arr = {5, 2, 8, 1, 3}; 
+// vector<int> arr = {5, 2, 8, 1, 3}; 
 
-sort(arr.begin(), arr.end()); 
-for(int x : arr) 
-    cout << x << " "; 
+// sort(arr.begin(), arr.end()); 
+// for(int x : arr) 
+//     cout << x << " "; 

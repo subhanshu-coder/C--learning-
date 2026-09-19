262f620a58ddbd78871c19918f4958d652363acc
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-19 23:33:52 +0530

    update all

--- switch.cpp
+++ switch.cpp
@@ -1,17 +1,17 @@
-// #include <iostream> 
-// using namespace std; 
+#include <iostream> 
+using namespace std; 
 
-// int day;
-// cin >> day;
+int day;
+cin >> day;
 
-// switch(day) {
-//     case 1: cout << "Monday"; break;
-//     case 2: cout << "Tuesday"; break;
-//     case 3: cout << "Wednesday"; break;
-//     case 4: cout << "Thursday"; break;
-//     case 5: cout << "Friday"; break;
-//     case 6: cout << "Saturday"; break;
-//     case 7: cout << "Sunday"; break;
-//     default: cout << "Invalid day";
-// return 0;  
-// } 
+switch(day) {
+    case 1: cout << "Monday"; break;
+    case 2: cout << "Tuesday"; break;
+    case 3: cout << "Wednesday"; break;
+    case 4: cout << "Thursday"; break;
+    case 5: cout << "Friday"; break;
+    case 6: cout << "Saturday"; break;
+    case 7: cout << "Sunday"; break;
+    default: cout << "Invalid day";
+return 0;  
+} 

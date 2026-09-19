cb9815c143c348c44be3cdaedaa122c2d30b7db6
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-19 23:29:16 +0530

    update all

--- var-datatype.cpp
+++ var-datatype.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
+// int main() {
      
-    int age = 22; 
-    bool isStudent = true; 
-    float height = 5.9; 
-    char grade = 'A';
-    double weight = 70.5; 
-    string name = "John"; 
-    cout << "my age is: " << age <<endl;
-    cout << "size of int: " << sizeof(age) << " bytes" << endl; 
-    cout << "size of bool: " << sizeof(isStudent) << " bytes" << endl; 
-    cout << "size of float: " << sizeof(height) << " bytes" << endl;
-    cout << "size of char: " << sizeof(grade) << " bytes" << endl;
-    cout << "size of double: " << sizeof(weight) << " bytes" << endl;
-    cout << "size of string: " << sizeof(name) << " bytes" << endl;
-    return 0;  
+//     int age = 22; 
+//     bool isStudent = true; 
+//     float height = 5.9; 
+//     char grade = 'A';
+//     double weight = 70.5; 
+//     string name = "John"; 
+//     cout << "my age is: " << age <<endl;
+//     cout << "size of int: " << sizeof(age) << " bytes" << endl; 
+//     cout << "size of bool: " << sizeof(isStudent) << " bytes" << endl; 
+//     cout << "size of float: " << sizeof(height) << " bytes" << endl;
+//     cout << "size of char: " << sizeof(grade) << " bytes" << endl;
+//     cout << "size of double: " << sizeof(weight) << " bytes" << endl;
+//     cout << "size of string: " << sizeof(name) << " bytes" << endl;
+//     return 0;  
     
     
-}
+// }
 
 
 
 
 
  
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main() {
-//    int a, b;
+int main() {
+   int a, b;
 
-//     cout << "Enter two numbers: ";
-//     cin >> a >> b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
 
-//     cout << "Sum = " << a + b;
-//   return 0;
-// }
+    cout << "Sum = " << a + b;
+  return 0;
+}

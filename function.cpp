132ca44f5bb37106861cd0976c6504de4e607af3
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-20 23:51:28 +0530

    update all

--- function.cpp
+++ function.cpp
@@ -1,11 +1,11 @@
-#include <iostream> 
-using namespace std; 
+// #include <iostream> 
+// using namespace std; 
 
-int add(int a, int b) { 
-    return a + b; 
-}
+// int add(int a, int b) { 
+//     return a + b; 
+// }
 
-int main() {
-    cout << add(10, 20);
-    return 0;
-}
+// int main() {
+//     cout << add(10, 20);
+//     return 0;
+// }

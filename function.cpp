7da3c2bbcc6704490f1a3ad4a4545d104efa81dd
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-22 02:23:51 +0530

    update all

--- function.cpp
+++ function.cpp
@@ -9,3 +9,16 @@ int main() {
     cout << add(10, 20);
     return 0;
 }
+
+int main1(){
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age >=18){
+        cout << "You are eligible to vote.";
+    } else {
+        cout << "You are not eligible to vote.";
+    }
+    return 0;
+}

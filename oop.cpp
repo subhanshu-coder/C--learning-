45bfa56c5467264af0b08bbaf37c88f9d8db4ccf
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-22 23:54:59 +0530

    update all

--- oop.cpp
+++ oop.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-class Student {
-public:
-    string name;
-    int age;
+// class Student {
+// public:
+//     string name;
+//     int age;
 
-    void display() {
-        cout << name << " " << age;
-    }
-};
-return 0;
-}
+//     void display() {
+//         cout << name << " " << age;
+//     }
+// };
+// return 0;
+// }
 int main() {
     Student student1;
     student1.name = "John";

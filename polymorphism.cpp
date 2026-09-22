206b3da5c4ac2b06746fe5c7b943ea47a142f9f8
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-22 23:56:17 +0530

    update all

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-class Student {  
-public: 
-    string name; 
+// class Student {  
+// public: 
+//     string name; 
 
-    Student(string n) { 
-        name = n;  
-    }
-};
+//     Student(string n) { 
+//         name = n;  
+//     }
+// };

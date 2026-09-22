e9678a080c60e69cfcb019802164cd26c475de0f
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-22 23:55:56 +0530

    update all

--- operators.cpp
+++ operators.cpp
@@ -1,12 +1,12 @@
-// #include <iostream> 
-// using namespace std;  
-// int main(){ 
-//     int a = 10, b = 3;
+#include <iostream> 
+using namespace std;  
+int main(){ 
+    int a = 10, b = 3;
 
-// cout << a + b << endl;
-// cout << a % b << endl;
-// cout << (a > b) << endl; << endl; 
-//     return 0;  
-// }  
+cout << a + b << endl;
+cout << a % b << endl;
+cout << (a > b) << endl; << endl; 
+    return 0;  
+}  
 
-// #include <iostream >
+#include <iostream >

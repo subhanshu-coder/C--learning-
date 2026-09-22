618178c24937090b5de4ca651c0651a1de740146
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-22 23:58:02 +0530

    update all

--- searching .cpp
+++ searching .cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int arr[] = {10, 20, 30, 40, 50};
-int target = 30;
+// int arr[] = {10, 20, 30, 40, 50};
+// int target = 30;
 
-for(int i = 0; i < 5; i++) {
-    if(arr[i] == target) {
-        cout << "Element found at index: " << i << endl;
-        break;
-    }
+// for(int i = 0; i < 5; i++) {
+//     if(arr[i] == target) {
+//         cout << "Element found at index: " << i << endl;
+//         break;
+//     }
 
 // binary search implementation
 #include <iostream>

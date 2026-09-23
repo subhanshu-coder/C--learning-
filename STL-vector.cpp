4fe3880eefaf39c60af7a97703fd648dac16dcc9
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-24 00:00:53 +0530

    update all

--- STL-vector.cpp
+++ STL-vector.cpp
@@ -1,11 +1,11 @@
-#include <vector>
-using namespace std;
+// #include <vector>
+// using namespace std;
 
-vector <int> nums ={1,2,3};
+// vector <int> nums ={1,2,3};
 
-nums.push_back(4);
+// nums.push_back(4);
 
 
-for(int x : nums) {
-    cout << x << " ";
-}
+// for(int x : nums) {
+//     cout << x << " ";
+// }
